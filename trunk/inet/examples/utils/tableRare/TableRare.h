14af472c2917d1bdb3f022d5e4cf24dc80e7de1c
rsimogaetan <rsimogaetan@87b9072a-e09f-11de-b41e-e93f42a66952>
2009-12-24 08:43:20 +0000

    INET est upgradé à sa dernière version

---
+++ trunk/inet/examples/utils/tableRare/TableRare.h
@@ -0,0 +1,14 @@
+#ifndef H_GL_TABLERARE
+#define H_GL_TABLERARE
+
+class TableRare {
+public:
+	TableRare(const char * name);
+	~TableRare();
+	void toString();
+private:
+	char * my_name;
+};
+
+#endif
+
