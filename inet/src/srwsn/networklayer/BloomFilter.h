d5a2bea32d8bdc49399b97284615353013af3d2d
rsimogaetan <rsimogaetan@87b9072a-e09f-11de-b41e-e93f42a66952>
2010-01-10 09:34:56 +0000

    BloomFilter up and running in the plateform !

--- inet/src/srwsn/networklayer/BloomFilter.h
+++ inet/src/srwsn/networklayer/BloomFilter.h
@@ -1,14 +1,25 @@
 #ifndef __BLOOMFILTER_H
 #define __BLOOMFILTER_H
 
+#include <cstdlib>
 #include <omnetpp.h>
+
+typedef unsigned int (*hashfunc_t)(const char *);
 
 class BloomFilter : public cSimpleModule
 {
 public:
-	BloomFilter();  // The Constructor
+	BloomFilter(){}  // The Constructor
+	BloomFilter(size_t size, size_t nfuncs, ...);  // Another Constructor
 	~BloomFilter();                  // The Destructor
 	void toString();				 // For information
+
+	size_t asize;
+	char *a;
+	size_t nfuncs;
+	hashfunc_t *funcs;
+	int Add(const char *s);
+	int Check(const char *s);
 private:
 	virtual void initialize();
 	bool ready;
